6cd452d81cee4234ab4ee270691c6c9e41ea22fb
Clément Thomas <clement.thomas@elneo.com>
2015-01-30 16:13:57 +0100

    Suppression de elneo_prospect (sera remplacé par un autre module)

--- elneo_prospect/elneo_prospect.py
+++
@@ -1,9 +0,0 @@
-from openerp import models,fields
-
-class res_partner(models.Model):
-    _name = 'res.partner'
-    _inherit = 'res.partner'
-
-    prospect = fields.Boolean('Prospect')
-    
-res_partner()

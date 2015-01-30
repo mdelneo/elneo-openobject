1506f600db681dd0d33ae1bdfc04e9f2970042a7
Clément Thomas <clement.thomas@elneo.com>
2015-01-30 11:59:20 +0100

    Ajout du module elneo_sale

---
+++ elneo_sale/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Elneo sale',
+    'version': '0.1',
+    'category': 'Sale',
+    'description': "Adapt sales flows to elneo specifics",
+    'author': 'Elneo',
+    'website': 'http://www.elneo.com',
+    'depends': [],
+    "data" : [
+        ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+}

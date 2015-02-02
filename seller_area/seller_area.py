25e4c411f277aeb018b72991cef8c9629d89e7c1
Denis Roussel <denis.roussel@elneo.com>
2015-02-02 12:02:56 +0100

    [ADD] Ajout des vues de réglage des secteurs de vente

--- seller_area/seller_area.py
+++ seller_area/seller_area.py
@@ -6,20 +6,16 @@ class user_dept_zip(models.Model):
     user = fields.Many2one('res.users', string='Seller', required=True)
     zip_min = fields.Char('Zip min', size=24, required=True)
     zip_max = fields.Char('Zip max', size=24, required=True)
-    department = fields.Many2one('rcrm.case.section',string='Sales Team', required=True)
+    department = fields.Many2one('crm.case.section',string='Sales Team', required=True)
 
 user_dept_zip()
 
-class partner_sale_exception(osv.osv):
+class partner_sale_exception(models.Model):
     _name = 'res.partner.sale.exception'
     
-    user = fields.Many2one('res.users')
-    
-    _columns = {
-        'user' : fields.many2one('res.users','User', required=True),
-        'department' : fields.many2one('crm.case.section','Sales Team', required=True),
-        'partner' : fields.many2one('res.partner','Partner', required=True),
-    }
+    user = fields.Many2one('res.users','User',required=True)
+    department = fields.Many2one('crm.case.section','Sales Team',required=True)
+    partner = fields.Many2one('res.partner','Partner',required=True)
     
 
 partner_sale_exception()

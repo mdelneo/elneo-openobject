3d6b0dbf1712dfc308673d32da91a9c23fb31332
Denis Roussel <denis.roussel@elneo.com>
2015-02-06 09:53:42 +0100

    [FIX] Onchange dans 'sale.order'

--- term_delivery/term_delivery.py
+++ term_delivery/term_delivery.py
@@ -1,7 +1,7 @@
 from openerp import models, fields
 
 class term_delivery(models.Model):
-    _inherit="account.payment.term"
+    _inherit='account.payment.term'
     
     default_order_policy = fields.Selection([
             ('prepaid', 'Payment Before Delivery'),
@@ -13,7 +13,7 @@ class term_delivery(models.Model):
   - The 'Pay Before delivery' choice will first generate the invoice and then generate the picking order after the payment of this invoice.
   - The 'Shipping & Manual Invoice' will create the picking order directly and wait for the user to manually click on the 'Invoice' button to generate the draft invoice.
   - The 'Invoice On Order After Delivery' choice will generate the draft invoice based on sales order after all picking lists have been finished.
-  - The 'Invoice From The Picking' choice is used to create an invoice during the picking process."""),
+  - The 'Invoice From The Picking' choice is used to create an invoice during the picking process.""")
     
 term_delivery()
 

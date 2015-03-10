14a4f83acbdaf6f6fcd8242ad318f7c48d0d48c6
elneo <elneo@ubuntu-dev>
2015-03-10 08:55:03 +0100

    [FIX] Supression du filtre 'Disponible pour les campagnes marketing'

--- elneo_crm/__openerp__.py
+++ elneo_crm/__openerp__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Elneo marketing',
+    'name': 'Elneo CRM',
     'version': '0.1',
     'category': 'Sale',
-    'description': "Adapt marketing to elneo specifics",
+    'description': "Adapt crm to elneo specifics",
     'author': 'Elneo',
     'website': 'http://www.elneo.com',
-    'depends': ['base','sale_crm'],
+    'depends': ['base','email_template','sale_crm'],
     "data" : ['views/elneo_crm_view.xml'],
     'installable': True,
     'auto_install': False,

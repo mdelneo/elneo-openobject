4421f031c6408c3905160a70728d957ac04e071d
elneo <elneo@ubuntu-dev>
2015-03-13 14:54:35 +0100

    test

---
+++ elneo_layout/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Elneo Layout',
+    'version': '0.1',
+    'category': 'Layout',
+    'description': "Adapt Sheet Width Larger",
+    'author': 'Elneo',
+    'website': 'http://www.elneo.com',
+    'depends': ['base','web','sale'],
+    "data" : ['views/sale_view.xml'
+        ],
+    'css':['static/src/css/elneo.css',
+        ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}

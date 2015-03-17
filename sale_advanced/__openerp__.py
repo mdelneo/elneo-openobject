3bf9af0e104b58cee6c04a3d3fef66b7faf9fccf
elneo <elneo@ubuntu-dev>
2015-03-17 12:07:11 +0100

    [ADD] Ajoute les champs picked_rate et invoiced_rate

---
+++ sale_advanced/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sales Advanced',
+    'version': '0.1',
+    'category': 'Sale',
+    'description': "Add functionalities to Sale Orders",
+    'author': 'Elneo',
+    'website': 'http://www.elneo.com',
+    'depends': ['sale'],
+    "data" : ['views/sale_view.xml'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}

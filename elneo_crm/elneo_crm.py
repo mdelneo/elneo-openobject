06d292d72fdf871d0c628fdeab5ad39a21727c5f
elneo <elneo@ubuntu-dev>
2015-03-23 10:49:20 +0100

    [FIX] Ajoute le champ référence dans les vues tree et form

--- elneo_crm/elneo_crm.py
+++ elneo_crm/elneo_crm.py
@@ -4,8 +4,11 @@ from openerp import models,fields,api
 class res_partner(models.Model):
     _inherit = 'res.partner'
     
+    ref = fields.Char('Reference', size=10,select=1, required=True)
+    
     def _get_default_is_company(self):
         return self._context.get('force_is_company', False)
         
     is_company = fields.Boolean('Is a company', default=_get_default_is_company)
-    
+
+res_partner()

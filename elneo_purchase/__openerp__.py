c8ecd971786d200c7419e4b100e915924a6617f9
droelneo <denis.roussel@elneo.com>
2015-04-02 10:48:24 +0200

    [ADD] Ajoute le lien vers les ventes liées à un achat ainsi que le type de commande d'achat

---
+++ elneo_purchase/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Elneo Purchase',
+    'version': '0.1',
+    'category': 'Purchase',
+    'description': "Adapt purchase to elneo specifics",
+    'author': 'Elneo',
+    'website': 'http://www.elneo.com',
+    'depends': ['purchase','sale'],
+    "data" : ['views/purchase_view.xml','security/ir.model.access.csv'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}
---
+++ elneo_purchase/purchase.py
@@ -0,0 +1,70 @@
+from openerp import models, fields, api
+
+
+class purchase_order_type(models.Model):
+    _name = 'purchase.order.type'
+    
+    name = fields.Char('Name',size=255,translate=True,required=True)
+    
+
+purchase_order_type()
+
+class purchase_order(models.Model):
+    
+    _inherit='purchase.order'
+    
+    def _get_sale_orders(self):
+        result = {}
+       
+        self.env.cr.execute('''select distinct purchase_order.id, sale_order.id
+                                from sale_order 
+                                left join sale_order_line
+                                    left join procurement_order
+                                        left join stock_move
+                                            left join procurement_order po2
+                                            left join purchase_order_line
+                                                left join purchase_order on purchase_order_line.order_id = purchase_order.id
+                                            on po2.purchase_line_id = purchase_order_line.id
+                                            on stock_move.id = po2.move_dest_id
+                                        on stock_move.procurement_id = procurement_order.id
+                                    on procurement_order.sale_line_id = sale_order_line.id
+                                on sale_order_line.order_id = sale_order.id
+                                where sale_order.state != 'cancel' and purchase_order.id in %s''',(tuple(self.mapped('id')),))
+        for (purchase_id, sale_id) in self.env.cr.fetchall():
+            self.sale_orders=[sale_id]
+            
+    
+    @api.depends('sale_orders')
+    def _count_all(self):
+        
+        self.sale_count=len(self.sale_orders)
+    
+    purchase_type_id = fields.Many2one('purchase.order.type','Purchase Type')
+    sale_orders = fields.Many2many(comodel_name='sale.order',compute=_get_sale_orders,string='Sale Orders',method=True)
+    sale_count = fields.Integer(compute=_count_all, method=True)
+    
+    @api.multi
+    def view_sale(self):
+        '''
+        This function returns an action that display existing sale orders of given purchase order ids.
+        It load the tree or the form according to the number of sale orders
+        '''
+        
+        mod_obj = self.env['ir.model.data']
+        dummy, action_id = tuple(mod_obj.get_object_reference('sale', 'action_order_tree'))
+        action_obj = self.env['ir.actions.act_window'].browse(action_id)
+        action = action_obj.read()[0]
+        
+
+        #override the context to get rid of the default filtering on picking type
+        action['context'] = {}
+        #choose the view_mode accordingly
+        if self.sale_count > 1:
+            action['domain'] = "[('id','in',[" + ','.join(map(str, self.sale_order.mapped('id'))) + "])]"
+        else:
+            res = mod_obj.get_object_reference('sale', 'view_order_form')
+            action['views'] = [(res and res[1] or False, 'form')]
+            action['res_id'] = self.sale_orders.mapped('id')[0] or False
+        return action
+    
+purchase_order()

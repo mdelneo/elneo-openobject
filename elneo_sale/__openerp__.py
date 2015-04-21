cdc2bde0546ac4758061a668f314077fc150d40b
elneo-cth <clement.thomas@elneo.com>
2015-04-21 06:10:09 +0200

    .

--- elneo_sale/__openerp__.py
+++ elneo_sale/__openerp__.py
@@ -7,11 +7,7 @@
     'description': "Adapt sales flows to elneo specifics",
     'author': 'Elneo',
     'website': 'http://www.elneo.com',
-<<<<<<< Upstream, based on branch '8.0' of https://github.com/Elneo-group/elneo-openobject
     'depends': ['base','sale','delivery','sale_margin','sale_crm','sales_team','elneo_crm','product','sale_stock'],
-=======
-    'depends': ['base','sale','sale_margin','sale_crm','sales_team','elneo_crm','product','sale_stock'],
->>>>>>> 0e71414 vue des ventes : ajout du tab historique et raccourcis en haut à droite
     "data" : ['views/elneo_sale_view.xml'
         ],
     'installable': True,

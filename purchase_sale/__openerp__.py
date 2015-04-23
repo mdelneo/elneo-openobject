db1b836910a9e775c51d67e19e1886912d554b29
droelneo <denis.roussel@elneo.com>
2015-04-23 09:59:08 +0200

    [ADD] Lien entre les acahts et les ventes

---
+++ purchase_sale/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Purchase Sale',
+    'version': '0.1',
+    'category': 'Purchase',
+    'description': "MAke the link between purchase and sales",
+    'author': 'Elneo',
+    'website': 'http://www.elneo.com',
+    'depends': ['purchase','sale'],
+    "data" : ['views/purchase_view.xml'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}

af1887fa3c27e81c38119fc25b84116dcbefa8b5
droelneo <denis.roussel@elneo.com>
2015-04-27 15:42:39 +0200

    [FIX]

--- elneo_crm/__openerp__.py
+++ elneo_crm/__openerp__.py
@@ -8,7 +8,7 @@
     'author': 'Elneo',
     'website': 'http://www.elneo.com',
     'depends': ['base','email_template','sale_crm'],
-    "data" : ['views/elneo_crm_view.xml',],
+    "data" : ['views/elneo_crm_view.xml', ],
     'installable': True,
     'auto_install': False,
     'application': True,

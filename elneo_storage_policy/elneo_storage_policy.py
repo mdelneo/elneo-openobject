4cbb655d85b0ad1c05013593e25db352fea3e881
elneo-cth <clement.thomas@elneo.com>
2015-06-17 08:29:40 +0200

    elneo_storage_policy : bugfix

--- elneo_storage_policy/elneo_storage_policy.py
+++ elneo_storage_policy/elneo_storage_policy.py
@@ -5,49 +5,73 @@ Created on 10 juil. 2012
 '''
 from datetime import datetime, timedelta
 from openerp import models, fields, api
+from openerp.osv.fields import related
 
 
 class product_warehouse_detail(models.Model):
     _name = 'product.warehouse.detail'
     
+    @api.one
     def compute_storage_policy(self):
-        result = {}    
-        for detail in self:
-            result[detail.id] = {}
-            if len([o.id for o in detail.product_id.orderpoints if detail.warehouse_id.id == o.warehouse_id.id]) > 0:
-                result[detail.id]['storage_policy'] = 'stocked'
-            elif detail.depreciation_policy == 'not_downgraded':
-                result[detail.id]['storage_policy'] = 'not_stocked'
-            elif detail.depreciation_policy == 'downgraded':
-                result[detail.id]['storage_policy'] = 'downgraded'
-            elif detail.depreciation_policy == 'drop':
-                result[detail.id]['storage_policy'] = 'drop'
-            else:
-                result[detail.id]['storage_policy'] = None
-            return result
-        
+        if len([o.id for o in self.product_tmpl_id.orderpoints if self.warehouse_id.id == o.warehouse_id.id]) > 0:
+            self.storage_policy = 'stocked'
+        elif self.depreciation_policy == 'not_downgraded':
+            self.storage_policy = 'not_stocked'
+        elif self.depreciation_policy == 'downgraded':
+            self.storage_policy = 'downgraded'
+        elif self.depreciation_policy == 'drop':
+            self.storage_policy = 'drop'
+        else:
+            self.storage_policy = None
+
+    @api.one
     def compute_stock(self):
-        self.stock_real = 0
-        self.stock_virtual = 0
+        if self.product_tmpl_id.id:
+            self.stock_real = self.product_tmpl_id.with_context({'location':self.warehouse_id.lot_stock_id.id})._product_available(None, False)[self.product_tmpl_id.id]['qty_available']
+            self.stock_virtual = self.product_tmpl_id.with_context({'location':self.warehouse_id.lot_stock_id.id})._product_available(None, False)[self.product_tmpl_id.id]['virtual_available']
     
-    product_id = fields.Many2one('product.template', string='Product')
-    warehouse_id = fields.Many2one('stock.warehouse')
+    product_tmpl_id = fields.Many2one('product.template', string='Product', ondelete='cascade')
+    warehouse_id = fields.Many2one('stock.warehouse', string='Warehouse')
     stock_real = fields.Float('Real stock', compute='compute_stock')
     stock_virtual = fields.Float('Virtual stock', compute='compute_stock')
     aisle = fields.Char('Aisle')
     storage_policy = fields.Selection([('stocked', 'Stocked'),('not_stocked','Not stocked'),('downgraded','Downgraded'),('drop','Drop')], string='Storage policy', compute='compute_storage_policy')
     depreciation_policy = fields.Selection([('not_downgraded', 'Not downgraded'), ('downgraded', 'Downgraded'), ('drop', 'Drop')], string='Depreciation policy', default='not_downgraded')
     warehouse_description = fields.Text('Warehouse description')
+    
 product_warehouse_detail()   
+
+class product_product(models.Model):
+    _inherit = 'product.product'
+    
+product_product()
+
+class stock_warehouse_orderpoint(models.Model):
+    _inherit = 'stock.warehouse.orderpoint'
+    
+    product_tmpl_id = fields.Many2one('product.template', related='product_id.product_tmpl_id', store=True)
+    
+stock_warehouse_orderpoint()
 
 class product_template(models.Model):
     _inherit = 'product.template'
+    
+    @api.model
+    def _get_default_warehouse_detail(self):
+        res = []
+        for warehouse in self.env['stock.warehouse'].search([]):
+            res.append({'warehouse_id':warehouse.id})
+        return res
+        
+    
     def copy(self, cr, uid, id, default=None, context=None):
         if not default:
             default = {}
         default['barcode_number'] = self.pool.get('ir.sequence').get(cr, uid, 'product.barcode')
         return super(product_template,self).copy(cr, uid, id, default=default, context=context)
     barcode_number = fields.Char('Barcode number', size=7, default=lambda obj: obj.env['ir.sequence'].get('product.barcode'))
-    warehouse_detail = fields.One2many('product.warehouse.detail', 'product_id', 'Warehouse detail')
-    orderpoints = fields.One2many('stock.warehouse.orderpoint', 'product_id', 'Minimum Inventory Rules')
+    warehouse_detail = fields.One2many('product.warehouse.detail', 'product_tmpl_id', 'Warehouse detail', default=_get_default_warehouse_detail)
+    orderpoints = fields.One2many('stock.warehouse.orderpoint', 'product_tmpl_id', 'Minimum Inventory Rules')
+    
+    
 product_template()

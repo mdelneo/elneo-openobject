2645457eb1b82e0d83918be4bcbdb124fadefa61
dro <dro@elneo.com>
2015-07-17 16:27:54 +0200

    [ADD] Première Release

---
+++ maintenance_project_quotation/maintenance_project_quotation_user.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Elneo
+#    Copyright (C) 2011-2015 Elneo (Technofluid SA) (<http://www.elneo.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields
+
+class users(models.Model):
+    _inherit = 'res.users'
+    _description = "Users"
+    
+    receive_project_quotation_request=fields.Boolean('Receive maintenance project quotation request',help='If you want to receive a mail notification when somebody wants an offer for a maintenance project.')

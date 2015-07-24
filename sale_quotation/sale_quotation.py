a8e2947589a372b07f07c83f66d04b49ba623224
elneo-cth <clement.thomas@elneo.com>
2015-07-24 11:04:38 +0200

    Toutes options de sale_quotation implémentées

--- sale_quotation/sale_quotation.py
+++ sale_quotation/sale_quotation.py
@@ -4,6 +4,7 @@ import math
 from openerp import models,fields,api
 from operator import itemgetter
 import re
+from pygments.lexer import _inherit
 
 
 class sale_quotation_text_element(models.Model):
@@ -154,3 +155,25 @@ class sale_order(models.Model):
     is_quotation = fields.Boolean("Is Quotation")
     
 sale_order()
+
+
+class sale_order_line(models.Model):
+    _inherit = 'sale.order.line'
+    
+    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
+            uom=False, qty_uos=0, uos=False, name='', partner_id=False,
+            lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, shop_id=0, context={}):
+        
+        res = super(sale_order_line, self).product_id_change(cr, uid, ids, pricelist, product, qty=qty,
+            uom=uom, qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
+            lang=lang, update_tax=update_tax, date_order=date_order, packaging=packaging, fiscal_position=fiscal_position, flag=flag, context=context)
+        
+        if not flag:
+            if not res['value']:
+                res['value'] = {}
+            product_obj = self.pool.get("product.product").browse(cr, uid, product, context)
+            context_partner = {'lang': lang, 'partner_id': partner_id}
+            res['value']['name'] = self.pool.get('product.product').name_get(cr, uid, [product_obj.id], context=context_partner)[0][1]
+        return res
+    
+sale_order_line()

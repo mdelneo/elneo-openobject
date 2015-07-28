192f3704ef279cbe5c7ce820b7a55f4eeb09f030
elneo-cth <clement.thomas@elneo.com>
2015-07-28 10:49:34 +0200

    Ajout des quotation drive link au rapport sale_quotation

--- sale_quotation_drive_link/sale_quotation_drive_link.py
+++ sale_quotation_drive_link/sale_quotation_drive_link.py
@@ -15,15 +15,12 @@ class sale_drive_link(models.Model):
         if self.product_drive_link_id:
             return self.product_drive_link_id
         
-    @api.one
-    def compute_name(self):
-        self.name = self.get_drive_link().name 
-    
-    name = fields.Char('Name',size=255,compute='compute_name')
+    name = fields.Char('Name',size=255)
+    link = fields.Char('Url',size=255)
     sale_order_id = fields.Many2one('sale.order', 'Sale order')    
     product_category_drive_link_id = fields.Many2one('product.category.drive.link', 'Drive link (category)')
     product_drive_link_id = fields.Many2one('product.category.drive.link', 'Drive link (product)')
-    sale_line_id = fields.Many2one('sale.order.line', 'Sale order line')
+    product_ids = fields.Many2many('product.product', 'product_drive_link_rel','drive_link_id','product_id','Products')
     
 sale_drive_link()
 
@@ -35,28 +32,54 @@ class sale_order(models.Model):
     
     @api.onchange('order_line')
     def on_change_order_lines(self):
+        def _drive_link_contains_cat(product_category_drive_link_id):
+            for drive_link in drive_links:
+                if drive_link.get('product_category_drive_link_id',0) == product_category_drive_link_id:
+                    return drive_link
+            return False
+        
+        def _drive_link_contains_prod(product_drive_link_id):
+            for drive_link in drive_links:
+                if drive_link.get('product_drive_link_id',0) == product_drive_link_id:
+                    return drive_link
+            return False
+        
+        
         drive_links = []
         for order_line in self.order_line:
             #create drive links from product_drive_links
             for drive_link in order_line.product_id.drive_links:
-                drive_links.append(
-                    {
-                     'sale_order_id':self.id, 
-                     'product_drive_link_id':drive_link.id,
-                     'sale_line_id':order_line.id
-                    }                   
-                )
+                existing_drive_link = _drive_link_contains_prod(drive_link.id)
+                if not existing_drive_link:
+                    drive_links.append(
+                        {
+                         'name':drive_link.name,
+                         'link':drive_link.link,
+                         'sale_order_id':self.id, 
+                         'product_drive_link_id':drive_link.id,
+                         'product_ids':[order_line.product_id.id]
+                        }                   
+                    )
+                else:
+                    existing_drive_link['product_ids'].append(order_line.product_id.id)
+                    
                 
             for drive_link in order_line.product_id.get_product_category_drive_links():
-                drive_links.append(
-                    {
-                     'sale_order_id':self.id, 
-                     'product_category_drive_link_id':drive_link.id,
-                     'sale_line_id':order_line.id
-                    }
-                )
+                existing_drive_link = _drive_link_contains_cat(drive_link.id)
+                if not existing_drive_link:
+                    drive_links.append(
+                        {
+                         'name':drive_link.name,
+                         'link':drive_link.link,
+                         'sale_order_id':self.id, 
+                         'product_category_drive_link_id':drive_link.id,
+                         'product_ids':[order_line.product_id.id]
+                        }
+                    )
+                else:
+                    existing_drive_link['product_ids'].append(order_line.product_id.id)
+                    
         self.drive_links = drive_links
-        
     
 sale_order()
 

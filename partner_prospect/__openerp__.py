5beeaf37552e31aea768433cb199c1e102261f25
elneo-cth <clement.thomas@elneo.com>
2015-08-24 15:17:01 +0200

    Ajout du module customer_prospect

---
+++ partner_prospect/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Partner prospect',
+    'version': '0.1',
+    'category': 'CRM',
+    'description': "Add type of partner : prospect",
+    'author': 'Elneo',
+    'website': 'http://www.elneo.com',
+    'depends': ['base',],
+    'data': ['view/partner_prospect_view.xml'],
+    'installable': True,
+    'active': False,
+    'application':False
+}

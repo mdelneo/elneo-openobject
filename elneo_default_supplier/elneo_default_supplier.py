b60d24052573bef851d3ec4f2711ca78436fda66
dro <dro@elneo.com>
2015-08-25 15:38:03 +0200

    [FIX] Erreur de compute

--- elneo_default_supplier/elneo_default_supplier.py
+++ elneo_default_supplier/elneo_default_supplier.py
@@ -1,18 +1,30 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Elneo
+#    Copyright (C) 2011-2015 Elneo (Technofluid SA) (<http://www.elneo.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 from openerp import models,fields,api
-from openerp.tools.float_utils import float_compare, float_round
-from datetime import datetime
-from openerp.exceptions import except_orm, AccessError, MissingError, ValidationError
-import logging
-import sys
-from operator import itemgetter, attrgetter
-from pygments.lexer import _inherit
-
-    
+   
 class product_template(models.Model):
     _inherit = "product.template"
     
     @api.multi
+    @api.depends('seller_ids')
     def get_default_supplier(self):
         for product in self:
             if len(product.seller_ids) == 1:
@@ -44,18 +56,15 @@ class product_template(models.Model):
             result.append(orderpoint.product_id.id)
         return result
     
-    default_supplier_id = fields.Many2one('res.partner', 'Default supplier', compute=get_default_supplier, method=True,readonly=True, store=True)
-    default_supplierinfo_id = fields.Many2one('product.supplierinfo', 'Default supplier', compute=get_default_supplierinfo, method=True, readonly=True)
-    
-product_template()
+    default_supplier_id = fields.Many2one('res.partner', 'Default supplier', compute=get_default_supplier,readonly=True, store=True)
+    default_supplierinfo_id = fields.Many2one('product.supplierinfo', 'Default supplier', compute=get_default_supplierinfo, readonly=True)
 
 
-class sale_order(models.Model):
+class sale_order_line(models.Model):
     _inherit = 'sale.order.line'
     
     #'default_supplier_id':fields.related("product_id","product_tmpl_id", "default_supplier_id", type="many2one", relation="res.partner", string="Default supplier", readonly=True),
     
     default_supplier_id = fields.Many2one('res.partner',related='product_id.product_tmpl_id.default_supplier_id',readonly=True,string='Default Supplier')
-    
-sale_order()
+
     

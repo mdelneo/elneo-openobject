b81f498d7853c19a31d33ed019800d2eeed74f96
elneo-cth <clement.thomas@elneo.com>
2015-10-09 15:05:24 +0200

    Gestion des count dans les partenaires (vue form : nb d'achats, invoice, etc..)

--- elneo_sale/elneo_sale.py
+++ elneo_sale/elneo_sale.py
@@ -45,12 +45,11 @@ class procurement_order(models.Model):
 class purchase_order(models.Model):
     _inherit = 'purchase.order'
     
-    @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
+    def search(self, cr, uid, args, offset=0, limit=None, order=None, count=False, context={}):
         #if make_po = True, we simulate that no other purchase order exists, to force new purchase order creation
-        if self._context.get('make_po',False):
+        if context.get('make_po',False):
             return self
-        return super(purchase_order, self).search(args, offset=offset, limit=limit, order=order, count=count)
+        return super(purchase_order, self).search(cr, uid, args, offset=offset, limit=limit, order=order, count=count, context=context)
     
 class sale_order_line(models.Model):
     _inherit = 'sale.order.line'

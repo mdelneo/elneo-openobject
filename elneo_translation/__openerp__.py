caa13b1c61fddab0d32d7e2c2bd42399e5a635ef
elneo-cth <clement.thomas@elneo.com>
2015-10-20 14:12:37 +0200

    Ajout module elneo_translation

---
+++ elneo_translation/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Elneo translation',
+    'version': '0.1',
+    'category': 'Sale',
+    'description': "Change translations",
+    'author': 'Elneo',
+    'website': 'http://www.elneo.com',
+    'depends': ['base','stock','product'],
+    "data" : [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}

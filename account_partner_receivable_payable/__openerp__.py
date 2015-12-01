91c6db46a4e4dac720307fd76d824884d5344b5f
dro <dro@elneo.com>
2015-12-01 14:54:40 +0100

    [ADD] Créditeurs / Débiteurs pour les partenaires

---
+++ account_partner_receivable_payable/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account Receivable/Payable in Partner',
+    'version': '0.1',
+    'category': 'Accounting',
+    'description': "Link to payable/receivable in partner",
+    'author': 'Elneo',
+    'website': 'http://www.elneo.com',
+    'depends': ['account'],
+    'data': ['views/partner_view.xml'],
+    'installable': True,
+    'active': False,
+    'application':False
+}

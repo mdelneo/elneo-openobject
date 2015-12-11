d882b1c4846a507a03d3e3ac06246c6a9c37713b
elneo-cth <clement.thomas@elneo.com>
2015-12-11 16:26:56 +0100

    Correction workflow shopsale + remise globale focus

--- elneo_maintenance_serial_number/wizard/serial_number_wizard.py
+++ elneo_maintenance_serial_number/wizard/serial_number_wizard.py
@@ -9,17 +9,18 @@ class sale_order(models.Model):
     def shop_sale_ship(self):
         for line in self.order_line:
             for element in line.maintenance_element_ids:
-                for move in self.picking_ids.filtered(lambda r:r.picking_type_id.code=='outgoing').move_lines:
-                    if move.product_id == element.product_id:
-                        if element.product_id.unique_serial_number:
-                            moves = self._split_move(move)
-                            
-                            if len(moves) != len(element.serial_number.split(';')):
-                                raise Warning(_('The quantity to deliver is not the same as serial numbers for the product : %s' % line.product_id.default_code))
-                            for new_move in moves:
-                                self._assign_serial(new_move, element.serial_number, element.product_id)
-                        else:
-                            self._assign_serial(move, element.serial_number, element.product_id)
+                for pick in self.picking_ids.filtered(lambda r:r.picking_type_id.code=='outgoing'):
+                    for move in pick.move_lines:
+                        if move.product_id == element.product_id:
+                            if element.product_id.unique_serial_number:
+                                moves = self._split_move(move)
+                                
+                                if not element.serial_number or len(moves) != len(element.serial_number.split(';')):
+                                    raise Warning(_('The quantity to deliver is not the same as serial numbers for the product : %s' % line.product_id.default_code))
+                                for new_move in moves:
+                                    self._assign_serial(new_move, element.serial_number, element.product_id)
+                            else:
+                                self._assign_serial(move, element.serial_number, element.product_id)
                             
         return super(sale_order,self).shop_sale_ship()
     

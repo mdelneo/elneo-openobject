c2836c652f4f59e20c506856a0f1a5a5828e6ee4
dro <dro@elneo.com>
2016-01-13 09:38:38 +0100

    [FIX] Cosmétique update_xml => data

--- elneo_supplier_price_update/__openerp__.py
+++ elneo_supplier_price_update/__openerp__.py
@@ -7,7 +7,7 @@
     'author': 'Elneo',
     'website': 'http://www.elneo.com',
     'depends': ['base','account','purchase','product','elneo_autocompute_webshop'],
-    "update_xml" : ['price_update.xml','price_update_sequence.xml','price_update_workflow.xml','price_update_line_workflow.xml','security/ir.model.access.csv','elneo_landefeld_price_update.xml', 'price_update_data.xml','product_view.xml','wizard/supplier_price_update_wizard_view.xml'],
+    "data" : ['price_update.xml','price_update_sequence.xml','price_update_workflow.xml','price_update_line_workflow.xml','security/ir.model.access.csv','elneo_landefeld_price_update.xml', 'price_update_data.xml','product_view.xml','wizard/supplier_price_update_wizard_view.xml'],
     'installable': True,
     'active': False,
 }

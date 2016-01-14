83b75c44d121af56d06ed9de571851f687b6390c
elneo-cth <clement.thomas@elneo.com>
2016-01-14 16:05:05 +0100

    a

--- elneo_sale/elneo_sale.py
+++ elneo_sale/elneo_sale.py
@@ -164,7 +164,7 @@ class sale_order(models.Model):
     @api.returns('self')
     def search(self, cr, user, args, offset=0, limit=None, order=None, context=None, count=False):
         #if my_dpt : display picking of sale teams of current user. If user is not linked to a sale team, display all picks
-        section_ids = self.pool.get('crm.case.section').search(cr, user, [('member_ids','in',self._uid)], context=context)
+        section_ids = self.pool.get('crm.case.section').search(cr, user, [('member_ids','in',user)], context=context)
         if context.get('my_dpt',True) and section_ids:
             args.append(('section_id','in',section_ids)) 
         res = super(sale_order, self).search(cr, user, args, offset=offset, limit=limit, order=order, context=context, count=count)

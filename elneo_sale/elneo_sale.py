4914fbd8e5ecffb9322058a246c5388994677d69
elneo-cth <clement.thomas@elneo.com>
2016-01-15 15:17:40 +0100

    correction script search ventes

--- elneo_sale/elneo_sale.py
+++ elneo_sale/elneo_sale.py
@@ -165,7 +165,7 @@ class sale_order(models.Model):
     def search(self, cr, user, args, offset=0, limit=None, order=None, context=None, count=False):
         #if my_dpt : display picking of sale teams of current user. If user is not linked to a sale team, display all picks
         section_ids = self.pool.get('crm.case.section').search(cr, user, [('member_ids','in',user)], context=context)
-        if context.get('my_dpt',True) and section_ids:
+        if context.get('my_dpt',False) and section_ids:
             args.append(('section_id','in',section_ids)) 
         res = super(sale_order, self).search(cr, user, args, offset=offset, limit=limit, order=order, context=context, count=count)
         return res

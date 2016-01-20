8055c9b2337e0f695411ed112780631a4d00294b
dro <dro@elneo.com>
2016-01-20 16:53:01 +0100

    [ADD] Qweb fix

---
+++ qweb_fix/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'QWEB Fix',
+    'version': '0.1',
+    'category': 'Elneo',
+    'description': "Some fixes to qweb management",
+    'author': 'Elneo',
+    'website': 'http://www.elneo.com',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'active': False,
+}

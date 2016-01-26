9d4f4affbc626a0dacf65a6e0835c3962f2af8ed
elneo-cth <clement.thomas@elneo.com>
2016-01-26 08:26:40 +0100

    modif crm view

--- elneo_crm/elneo_crm.py
+++ elneo_crm/elneo_crm.py
@@ -88,7 +88,7 @@ class calendar_event(models.Model):
             if len(self.env['res.users'].search([('partner_id','=',p.id)])) > 0:
                 continue
             if p.commercial_partner_id and p.commercial_partner_id.id != p.id and p.commercial_partner_id.name:
-                prefix = prefix + p.commercial_partner_id.name
+                prefix = prefix + p.commercial_partner_id.name+' '
             if p.name:
                 prefix = prefix + p.name
             #set address

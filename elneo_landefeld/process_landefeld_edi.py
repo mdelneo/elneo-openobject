e0325f4a0f2710b3fe67b0f6670acd7612b56017
dro <dro@elneo.com>
2016-02-05 10:53:25 +0100

    [FIX] Gestion globale des exceptions

--- elneo_landefeld/process_landefeld_edi.py
+++ elneo_landefeld/process_landefeld_edi.py
@@ -10,37 +10,46 @@ class EDIProcessor(models.Model):
     
     @api.model
     def _process(self,messages):
-        for message in messages.filtered(lambda r:r.type.processor.id==self.id and r.state=='confirmed'):
-            attachments = self.env['ir.attachment'].search([('res_model','=','edi.message'),('res_id','=',message.id)])
-            for attachment in attachments:
-                document = self.createFromOpenTransXML(base64.decodestring(attachment.datas))
-                
-                wizard = self.env['edi.processor.landefeld'].create({'edi_message':message.id,'edi_processor':self.id})
-                
-                wizard._process_landefeld_document(document)
         
-                if wizard.warning_message:
-                    message.message_post( 
-                        body = _("A warning has occured during Landefeld EDI document processing.") + " <b><a href='#id=" + str(message.id) + "&view_type=form&model=edi.message'> " + message.name + " </a></b><br/><br/>" + wizard.warning_message,
-                        type = 'mail',
-                        subtype = "mail.mt_comment",
-                        model = 'edi.message', res_id = message.id, 
-                        partner_ids = [self.send_warning_users])
+        for message in messages.filtered(lambda r:r.type.processor.id==self.id and r.state=='confirmed'):
+            try:
+                
+                attachments = self.env['ir.attachment'].search([('res_model','=','edi.message'),('res_id','=',message.id)])
+                for attachment in attachments:
+                    document = self.createFromOpenTransXML(base64.decodestring(attachment.datas))
                     
-                if wizard.error_message:
-                    message.message_post( 
-                        body = _("An error has occured during Landefeld EDI document processing.") + " <b><a href='#id=" + str(message.id) + "&view_type=form&model=edi.message'> " + message.name + " </a></b><br/><br/>" + wizard.error_message,
-                        type = 'mail',
-                        subtype = "mail.mt_comment",
-                        model = 'edi.message', res_id = message.id, 
-                        partner_ids = [self.send_error_users])
+                    wizard = self.env['edi.processor.landefeld'].create({'edi_message':message.id,'edi_processor':self.id})
                     
-                if wizard.state in ['ok','warning']:
-                    message.state = 'done'
-                elif wizard.state == 'error':
-                    message.state = 'error'
-             
+                    wizard._process_landefeld_document(document)
             
-            
+                    if wizard.warning_message:
+                        message.message_post( 
+                            body = _("A warning has occured during Landefeld EDI document processing.") + " <b><a href='#id=" + str(message.id) + "&view_type=form&model=edi.message'> " + message.name + " </a></b><br/><br/>" + wizard.warning_message,
+                            type = 'email',
+                            subtype = "mail.mt_comment",
+                            model = 'edi.message', res_id = message.id, 
+                            partner_ids = [self.send_warning_users])
+                        
+                    if wizard.error_message:
+                        message.message_post( 
+                            body = _("An error has occured during Landefeld EDI document processing.") + " <b><a href='#id=" + str(message.id) + "&view_type=form&model=edi.message'> " + message.name + " </a></b><br/><br/>" + wizard.error_message,
+                            type = 'email',
+                            subtype = "mail.mt_comment",
+                            model = 'edi.message', res_id = message.id, 
+                            partner_ids = [self.send_error_users])
+                        
+                    if wizard.state in ['ok','warning']:
+                        message.state = 'done'
+                    elif wizard.state == 'error':
+                        message.state = 'error'
+            except Exception,e:
+                message.state='error'
+                message.message_post( 
+                            body = _("An error has occured during Landefeld EDI document processing.") + " <b><a href='#id=" + str(message.id) + "&view_type=form&model=edi.message'> " + message.name + " </a></b><br/><br/>" + unicode(e.message),
+                            type = 'email',
+                            subtype = "mail.mt_comment",
+                            model = 'edi.message', res_id = message.id, 
+                            partner_ids = [self.send_error_users])
+ 
     
         return super(EDIProcessor,self)._process(messages)        

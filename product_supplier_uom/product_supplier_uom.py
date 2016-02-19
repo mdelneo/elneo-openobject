fd528a6138981e16614dcb119b121612cf1a6594
elneo-cth <clement.thomas@elneo.com>
2016-02-19 11:35:16 +0100

    correction bug dupliaction produit

--- product_supplier_uom/product_supplier_uom.py
+++ product_supplier_uom/product_supplier_uom.py
@@ -6,9 +6,9 @@ class ProductSupplierinfo(models.Model):
 
     @api.model
     def _get_supplier_uom(self):
-        product_tmpl_id = self.env['product.template'].browse(self.env.context.get('product_tmpl_id',False))
-        if product_tmpl_id and product_tmpl_id.uom_po_id:
-            return product_tmpl_id.uom_po_id
+        product = self.env['product.product'].browse(self.env.context.get('product_id',False))
+        if product and product.uom_po_id:
+            return product.uom_po_id
     
     product_uom = fields.Many2one(comodel_name='product.uom',default=_get_supplier_uom,required=True,readonly=False,help="This comes from the product form by default. But you can provide a specific uom for one supplier")
     

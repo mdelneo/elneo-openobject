8067d82f64ca6daa9033dd0a704c189b2139e9e9
FrancoisHodElneo <francois.hodiaumont@elneo.com>
2016-02-29 13:35:20 +0100

    [ADD] Vues Elneo

---
+++ elneo_views/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Elneo views',
+    'version': '0.1',
+    'category': '',
+    'description': "Adapt views to elneo specifics",
+    'author': 'Elneo',
+    'website': 'http://www.elneo.com',
+    'depends': [],
+    "data" : [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}

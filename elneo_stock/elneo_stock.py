02905f4b1fef84c5e2c4e1d4d0301e35e09356a4
dro <dro@elneo.com>
2016-03-09 22:02:02 +0100

    [FIX] Mauvaises fonctions

--- elneo_stock/elneo_stock.py
+++ elneo_stock/elneo_stock.py
@@ -61,36 +61,8 @@ class procurement_order(models.Model):
         return {}
     
 
-class sale_order_line(models.Model):
-    _inherit = 'sale.order.line'
 
-    @api.multi
-    def product_id_change_with_wh(self, pricelist=False, product=False, qty=0,
-            uom=False, qty_uos=0, uos=False, name='', partner_id=False,
-            lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, warehouse_id=False):
-        
-        res = super(sale_order_line, self).product_id_change_with_wh(pricelist, product, qty, uom, qty_uos, uos, name, partner_id, lang, update_tax, date_order, packaging, fiscal_position, flag, warehouse_id)
-        
-        product = self.env['product.product'].browse(product)
-        warehouse = self.env['stock.warehouse'].browse(warehouse_id)
-        
-        if product:
-            current_stock = product.with_context({'location':warehouse.lot_stock_id.id})._product_available()[product.id]
-            if current_stock and ('qty_available' in current_stock):
-                qty_in_stock = current_stock['qty_available']
-            else:
-                qty_in_stock = 0
-                
-            qty_other_wh = {}
-            for other_wh in warehouse.resupply_wh_ids:
-                qty_other_wh[other_wh] = product.with_context({'location':other_wh.lot_stock_id.id})._product_available()[product.id]['qty_available']
-            
-            if qty_in_stock <= 0:
-                ""
-        return res
     
-sale_order_line()
-
 class stock_picking(models.Model):
     _inherit = 'stock.picking'
     

bdb87140e907e80473dfde52b3e6be85043cf270
FrancoisHodElneo <francois.hodiaumont@elneo.com>
2016-03-18 16:26:53 +0100

    [ADD] Boutons de mise a jour de l'intervention

--- elneo_views/__openerp__.py
+++ elneo_views/__openerp__.py
@@ -6,7 +6,7 @@
     'description': "Adapt views to elneo specifics",
     'author': 'Elneo',
     'website': 'http://www.elneo.com',
-    'depends': ['maintenance','elneo_maintenance','maintenance_timeofuse'],
+    'depends': ['maintenance','elneo_maintenance','maintenance_timeofuse', 'maintenance_product'],
     "data" : ['views/maintenance_view.xml'],
     'installable': True,
     'auto_install': False,
